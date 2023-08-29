e88554788eb4220bb049e277adce9cec69cb38d6
Giselle bernal <giselle.bernal@adidas.com>
2023-08-29 15:43:48 -0500

    First steps - counter and programming sequentially

---
+++ app.ts
@@ -0,0 +1,23 @@
+let numberOfLines = 1;
+
+console.log('linea #', numberOfLines);
+//TODO:
+// numberOfLines = numberOfLines + 1;
+// numberOfLines++;
+numberOfLines += 1;
+
+console.log('linea #', numberOfLines);
+//TODO:
+// numberOfLines = numberOfLines + 1;
+// numberOfLines++;
+numberOfLines += 1;
+
+console.log('linea #', numberOfLines);
+//TODO:
+// numberOfLines = numberOfLines + 1;
+// numberOfLines++;
+numberOfLines += 1;
+
+console.log('linea #', numberOfLines);
+
+
