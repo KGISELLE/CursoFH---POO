7be540342d3f15a2a8da9b356ecffe221e17b134
Giselle bernal <giselle.bernal@adidas.com>
2023-09-01 15:33:12 -0500

    if-else exercises

---
+++ sections/if-else.ts
@@ -0,0 +1,29 @@
+
+
+console.log('Inicio de programa');
+
+// let isTired: boolean = false;
+
+// if ( isTired === true ) {
+//     console.log('Tomar un café')
+// }
+// if ( isTired ) {
+//     console.log('Tomar un café')
+// } else {
+//     console.log('Tomar un agua')
+// }
+
+export let grade: number = 50;
+
+if( grade >= 60 ) {
+    console.log('El alumno aprueba la clase')
+} else if ( grade < 50 ) {
+    console.log('Por favor estudie mas');
+} else {
+    console.log('el alumno NO aprueba la clase');
+}
+
+console.log('Fin de programa');
+
+
+
